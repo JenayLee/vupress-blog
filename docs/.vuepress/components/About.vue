7135cef71aa88dfbb187d84781ff2b8d807d2bdc
Jenay_Lee <jenay.lee@hotmail.com>
2019-05-01 23:13:01 +0800

    [modified]自我介绍页面完善

--- docs/.vuepress/components/About.vue
+++ docs/.vuepress/components/About.vue
@@ -1,11 +1,101 @@
 <template>
   <div class='about-wrapper'>
-    <div class='about-paper-stock'>
-      <main class="about-paper clearfix">
-        <div class="about-paper-cont">
-
+    <div class="content">
+      <section class="selection brd-btm">
+        <div class="row">
+          <h2>ABOUT ME</h2>
+          <p>
+            <b>Helo, I’m Jenay Lee!</b>
+            <br>
+            Logo or your Company Name, unless of course you are Microsoft or the Yellow Pages online directorye core of
+            your marketing, the central theme around your products and services.
+            Your brand is not your Logo or your Company Name, unless of course you are Microsoft or the Yellow Pages
+            online directory.
+          </p>
         </div>
-      </main>
+      </section>
+      <section class="selection brd-btm">
+        <div class="row">
+          <div class="col-sm-6 clear-mrg">
+              <h2 class="title-thin text-muted">personal information</h2>
+
+              <dl class="dl-horizontal clear-mrg">
+                  <dt class="text-upper">Full Name</dt>
+                  <dd>Ola Maria Lowe</dd>
+
+                  <dt class="text-upper">D.o.b.</dt>
+                  <dd>05 Sep 1986</dd>
+
+                  <dt class="text-upper">address</dt>
+                  <dd>24058, Belgium, Brussels,Liutte 27, BE</dd>
+
+                  <dt class="text-upper">e-mail</dt>
+                  <dd><a href="mailto:robertsmith@company.com">robertsmith@company.com</a></dd>
+
+                  <dt class="text-upper">phone</dt>
+                  <dd>+1 256 254 84 56</dd>
+
+                  <dt class="text-upper">freelance</dt>
+                  <dd>Available</dd>
+              </dl>
+          </div><!-- .col-sm-6 -->
+
+          <div class="col-sm-6 clear-mrg">
+              <h2 class="title-thin text-muted">languages</h2>
+
+              <div class="progress-bullets ace-animated" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="10">
+                  <strong class="progress-title">English</strong>
+                  <span class="progress-bar">
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                  </span>
+                  <span class="progress-text text-muted">native</span>
+              </div>
+
+              <div class="progress-bullets ace-animated" role="progressbar" aria-valuenow="8" aria-valuemin="0" aria-valuemax="10">
+                  <strong class="progress-title">Spanish</strong>
+                  <span class="progress-bar">
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet"></span>
+                      <span class="bullet"></span>
+                  </span>
+                  <span class="progress-text text-muted">intermediate</span>
+              </div>
+
+              <div class="progress-bullets ace-animated" role="progressbar" aria-valuenow="7" aria-valuemin="0" aria-valuemax="10">
+                  <strong class="progress-title">Italian</strong>
+                  <span class="progress-bar">
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet fill"></span>
+                      <span class="bullet"></span>
+                      <span class="bullet"></span>
+                      <span class="bullet"></span>
+                  </span>
+                  <span class="progress-text text-muted">begginer</span>
+              </div>
+          </div><!-- .col-sm-6 -->
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -15,31 +105,91 @@
   width: 100%;
   height: 910px;
   position: relative;
+  letter-spacing: 0.08em;
+  font-family: "Quicksand", sans-serif;
+  font-size: 16px;
+  line-height: 1.4em;
 }
-.about-paper-stock{
-    max-width: 940px;
-    margin: 0 auto;
-    padding: 3.6rem 2rem 0;
-    border: 1px solid #eaecef;
-    position: relative;
-    top: 50px;
-    z-index: 0;
+.about-wrapper .content{
+  background-color: rgba(255, 255, 255, .6);
 }
-.about-paper-stock::before,.about-paper-stock::after{
-    content: '';
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    border: 1px solid #e6e6e6;
+.about-wrapper h2{
+  border-bottom: 0px;
 }
-.about-paper-stock:before {
-    top: 2px;
-    left: 2px;
-    z-index: 1;
+.section.brd-btm {
+    padding-bottom: 1.375em;
 }
-.about-paper-stock:after {
-    top: 5px;
-    left: 5px;
-    z-index: 0;
+.section {
+    margin-bottom: 2.188em;
+}
+.brd-btm {
+    border-bottom: 1px solid #e6e6e6;
+}
+.row:after {
+    clear: both;
+}
+.row:before, .row:after {
+  content: " ";
+  display: table;
+}
+@media (min-width: 768px){
+  .col-sm-6 {
+    width: 50%;
+  }
+  .dl-horizontal dt {
+    float: left;
+    width: 100px;
+    overflow: hidden;
+    clear: left;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+}
+@media (min-width: 768px){
+  .col-sm-1, 
+  .col-sm-2, 
+  .col-sm-3, 
+  .col-sm-4, 
+  .col-sm-5, 
+  .col-sm-6, 
+  .col-sm-7, 
+  .col-sm-8, 
+  .col-sm-9, 
+  .col-sm-10, 
+  .col-sm-11, 
+  .col-sm-12 {
+    float: left;
+  }
+}
+.col-sm-6{
+  position: relative;
+  min-height: 1px;
+}
+.post-content, .text-muted {
+    color: #757575;
+}
+
+.title-thin {
+    font-size: 1.35em;
+    font-weight: 400;
+    margin-bottom: 1.571em;
+}
+.clear-mrg > *:last-child {
+    margin-bottom: 0 !important;
+}
+
+dl {
+    font-size: 0.9em;
+    margin-top: 22px;
+    margin-bottom: 22px;
+}
+.text-upper {
+    text-transform: uppercase;
+}
+dt {
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 0.9em;
+    letter-spacing: 0.01em;
 }
 </style>
